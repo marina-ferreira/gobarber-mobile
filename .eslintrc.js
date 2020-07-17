1767822e8c97b515f146f302ceb4cb4a7764984f
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-17 20:05:53 -0300

    Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,51 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hook',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'neve'
+      }
+    ]
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ['src'],
+        extensions: ['.jsx', '.js']
+      }
+    }
+  }
+}
