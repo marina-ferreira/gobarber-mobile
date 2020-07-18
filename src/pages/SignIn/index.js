8f4e878398fae120ede80e1423ed8ef45c98b4c0
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-18 13:40:04 -0300

    Sign in page styles

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Image } from 'react-native'
 
 import logo from 'assets/logo.png'
-import { Container } from './styles'
+import { Container, Title } from './styles'
 
 const SignIn = () => {
   return (
     <Container>
       <Image source={logo} />
+
+      <Title>Sign In</Title>
     </Container>
   )
 }
