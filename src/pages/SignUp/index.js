d77be3a6c83505204c0039d65d1e59e828380e26
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-18 16:50:41 -0300

    Sign up page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,53 @@
 import React from 'react'
-import { Container } from './styles'
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+
+import Input from 'components/Input'
+import Button from 'components/Button'
+
+import logo from 'assets/logo.png'
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
 
 const SignUp = () => {
-  return <Container />
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logo} />
+
+            <View>
+              <Title>Create an account</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Name" />
+            <Input name="email" icon="mail" placeholder="Email" />
+            <Input name="password" icon="lock" placeholder="Password" />
+
+            <Button>Sign Up</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn>
+        <Icon name="arrow-left" size={20} color="#f4ede8" />
+        <BackToSignInText>Back to sign in</BackToSignInText>
+      </BackToSignIn>
+    </>
+  )
 }
 
 export default SignUp
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,3 +1,34 @@
 import styled from 'styled-components/native'
+import { getBottomSpace } from 'react-native-iphone-x-helper'
 
-export const Container = styled.View``
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 30px;
+  justify-content: center;
+  align-items: center;
+`
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`
+export const BackToSignIn = styled.TouchableOpacity`
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 16px 0 ${getBottomSpace()}px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+`
+export const BackToSignInText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  color: #f4ede8;
+  margin-left: 16px;
+`
