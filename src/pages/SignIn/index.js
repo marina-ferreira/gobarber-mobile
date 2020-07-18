a192fa9e0a0346b12f6e0a2b568472595dc46cd8
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-18 16:39:20 -0300

    Sign in forget password and create account buttons

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,24 +1,62 @@
 import React from 'react'
-import { Image } from 'react-native'
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
 
 import Input from 'components/Input'
 import Button from 'components/Button'
 
 import logo from 'assets/logo.png'
-import { Container, Title } from './styles'
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+} from './styles'
 
 const SignIn = () => {
   return (
-    <Container>
-      <Image source={logo} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logo} />
 
-      <Title>Sign In</Title>
+            <View>
+              <Title>Sign In</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="Email" />
-      <Input name="password" icon="lock" placeholder="Password" />
+            <Input name="email" icon="mail" placeholder="Email" />
+            <Input name="password" icon="lock" placeholder="Password" />
 
-      <Button>Sign In</Button>
-    </Container>
+            <Button>Sign In</Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Forgot password</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create an account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   )
 }
 
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native'
+import { getBottomSpace } from 'react-native-iphone-x-helper'
 
 export const Container = styled.View`
   flex: 1;
@@ -11,4 +12,31 @@ export const Title = styled.Text`
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
   margin: 64px 0 24px;
+`
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Regular';
+  margin: 64px 0 24px;
+`
+export const CreateAccountButton = styled.TouchableOpacity`
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 16px 0 ${getBottomSpace()}px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+`
+export const CreateAccountButtonText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  color: #ff9000;
+  margin-left: 16px;
 `
