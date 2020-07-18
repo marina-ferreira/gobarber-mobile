c1c629758bb3e43ac9f59a34181e57436898c5d4
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-18 11:23:46 -0300

    Removes react native app initial code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 200 }}>Hello</Text>
+    </View>
+  )
+}
+
+export default App
