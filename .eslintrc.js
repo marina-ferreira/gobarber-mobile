04b00cf46af16fe3b8a4f32f6f8b7d703a962597
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-18 14:49:10 -0300

    Installs react-native-vector-icons

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
