dadda850169578a821a09ca734d498c76c1d9aa3
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-18 22:08:22 -0300

    Sign up errors

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -4,11 +4,15 @@ import {
   View,
   ScrollView,
   KeyboardAvoidingView,
-  Platform
+  Platform,
+  Alert
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
 import { Form } from '@unform/mobile'
+import * as Yup from 'yup'
+
+import getValidationErrors from 'utils/getValidationErrors'
 
 import Input from 'components/Input'
 import Button from 'components/Button'
@@ -16,14 +20,38 @@ import Button from 'components/Button'
 import logo from 'assets/logo.png'
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
 
+const schema = Yup.object().shape({
+  name: Yup.string().required(),
+  email: Yup.string().required().email(),
+  password: Yup.string().min(6)
+})
+
 const SignUp = () => {
   const navigation = useNavigation()
   const formRef = useRef(null)
   const emailInputRef = useRef(null)
   const passwordInputRef = useRef(null)
 
-  const handleSignUp = useCallback(data => {
-    console.log(data)
+  const handleSignUp = useCallback(async data => {
+    formRef.current && formRef.current.setErrors({})
+
+    try {
+      await schema.validate(data, { abortEarly: false })
+      // await api.post('/users', data)
+
+      // history.push('/')
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error)
+        formRef.current && formRef.current.setErrors(errors)
+        return
+      }
+
+      Alert.alert(
+        'Authentication Error',
+        'Sign in failed. Invalid credentials.'
+      )
+    }
   }, [])
 
   return (
