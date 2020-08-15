4593d7835a96cbb2f9195f4237e4e426e1e49af7
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-15 16:24:14 -0300

    Create appointment page header

--- src/pages/CreateAppointment/index.js
+++ src/pages/CreateAppointment/index.js
@@ -1,8 +1,41 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useCallback } from 'react'
+import { useRoute, useNavigation } from '@react-navigation/native'
+
+import Icon from 'react-native-vector-icons/Feather'
+
+import { useAuth } from 'hooks'
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar
+} from './styles'
 
 const CreateAppointment = () => {
-  return <View />
+  const { user } = useAuth()
+  const { goBack } = useNavigation()
+  const route = useRoute()
+  const { providerId } = route.params
+
+  const navigateBack = useCallback(() => {
+    goBack()
+  }, [goBack])
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Hair Stylists</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  )
 }
 
 export default CreateAppointment
